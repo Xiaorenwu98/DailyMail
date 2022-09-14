42a86697a16e475f0e33586056ffe982d6d3e029
筱仁恶 <93661742+Xiaorenwu98@users.noreply.github.com>
2022-09-14 22:18:02 +0800

    Update dailyMail.sql

--- dailyMail.sql
+++ dailyMail.sql
@@ -9,6 +9,6 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL COMMENT '姓名',
   `mail` varchar(255) NOT NULL COMMENT '邮箱',
-  `city` varchar(255) NOT NULL COMMENT '所在城市',
+  `city` varchar(255) NOT NULL COMMENT '地址',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
